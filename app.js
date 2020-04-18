38b6e9771cbff9933de4d561abd5853f1f5b47de
jls83 <josephlsanders@gmail.com>
2020-04-18 15:48:38 -0400

    Add sarcastify method

--- app.js
+++ app.js
@@ -1,9 +1,28 @@
-const updateResultBuilder = targetElement => {
+const sarcastify = (inStr, skipSpaces = false, letterSkip = 2) => {
+    const indexPredicate = i => i % letterSkip === 0;
+
+    if (skipSpaces) {
+        return inStr
+            .split(' ')
+            .map(word => sarcastify(word))
+            .join(' ');
+    }
+    return inStr
+        .toLowerCase()
+        .split('')
+        .map((char, i) => {
+            return indexPredicate(i) ? char.toUpperCase() : char;
+        })
+        .join('');
+};
+
+const updateResultBuilder = (targetElement, transformFunction) => {
     return event => {
         const value = event.target.value;
-        targetElement.innerText = value;
+        targetElement.innerText = transformFunction(value);
     };
 };
+
 
 window.onload = () => {
     const outerForm = document.getElementById('outerForm');
@@ -15,6 +34,6 @@ window.onload = () => {
         event.preventDefault();
     });
 
-    const updateResult = updateResultBuilder(resultDiv);
+    const updateResult = updateResultBuilder(resultDiv, sarcastify);
     textInput.addEventListener('input', updateResult);
 }
